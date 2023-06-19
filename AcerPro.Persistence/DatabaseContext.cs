3447e1ceb6156d6240f674fd024e4d243d7a6ad2
Arash Abbasi <h.abbasi@efarda.ir>
2023-06-19 15:49:14 +0330

    run with docker compose

--- AcerPro.Persistence/DatabaseContext.cs
+++ AcerPro.Persistence/DatabaseContext.cs
@@ -14,6 +14,7 @@ public class DatabaseContext : DbContext
     public DatabaseContext
         (DbContextOptions<DatabaseContext> options) : base(options)
     {
+        Database.Migrate();
     }
 
     public DbSet<User> Users { get; set; }
